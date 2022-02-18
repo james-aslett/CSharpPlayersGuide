e4500e7a88894c082c758aab7cde1ab9a9c35653
jamesa <james.aslett@directlife.co.uk>
2022-02-18 15:10:23 +0000

    Add project files.

---
+++ Challenges/TheVariableShop.cs
@@ -0,0 +1,43 @@
+﻿//int min -2147483648 max 2147483647
+int myInt = -2147483648;
+//short min -32768 max 32767
+short myShort = -32768;
+//long min 9223372036854775808 max 9223372036854775807
+long myLong = 9223372036854775807;
+//byte min 0 max 255
+byte myByte = 255;
+//sbyte min -128 max 127
+sbyte mySByte = -128;
+//uint min 0 max 4294967295
+uint myUint = 4294967295;
+//ushort min 0 max 65535
+ushort myUShort = 65535;
+//ulong min 0 max 18446744073709551615
+ulong myULong = 18446744073709551615;
+//float digits of precision 7
+float myFloat = 3.5623f;
+//double digits of precision 15-16
+double myDouble = 3.5623;
+//decimal digits of precision 28-29
+decimal myDecimal = 3.5623m;
+//char only holds single character
+char myChar = 'a';
+//string holds multiple characters
+string myString = "Hello";
+//bool true/false
+bool myBoolean = false;
+
+Console.WriteLine("My int value is " + myInt);
+Console.WriteLine("My short value is " + myShort);
+Console.WriteLine("My long value is " + myLong);
+Console.WriteLine("My byte value is " + myByte);
+Console.WriteLine("My sbyte value is " + mySByte);
+Console.WriteLine("My uint value is " + myUint);
+Console.WriteLine("My ushort value is " + myUShort);
+Console.WriteLine("My ulong value is " + myULong);
+Console.WriteLine("My float value is " + myFloat);
+Console.WriteLine("My double value is " + myDouble);
+Console.WriteLine("My decimal value is " + myDecimal);
+Console.WriteLine("My char value is " + myChar);
+Console.WriteLine("My string value is " + myString);
+Console.WriteLine("My boolean value is " + myBoolean);
