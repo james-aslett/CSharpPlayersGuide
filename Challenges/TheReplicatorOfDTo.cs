9fb6d35e19bcc8f0499d4f01a9a9b329ab15e70a
James Aslett <JamesA@directlife.co.uk>
2022-03-01 22:41:25 +0000

    Completed The Replicator Of D'To

---
+++ Challenges/TheReplicatorOfDTo.cs
@@ -0,0 +1,17 @@
+﻿int[] original = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    original[i] = number;
+}
+
+int[] copy = new int[5];
+
+for (int i = 0; i < 5; i++) copy[i] = original[i];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Original = {original[i]} and Copy = {copy[i]}");
+}
