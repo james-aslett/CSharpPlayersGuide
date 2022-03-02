804c9f470bb53a4f9f93f276798130093586fef5
jamesa <james.aslett@directlife.co.uk>
2022-03-02 13:03:07 +0000

    Completed The Laws of Freach

---
+++ Challenges/TheLawsOfFreach.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue;
+int total = 0;
+
+foreach (int value in array)
+{
+    if (value < currentSmallest)
+        currentSmallest = value;
+
+    total += value;
+}
+
+float average = (float)total / array.Length;
+
+Console.WriteLine($"The smallest value in the array is {currentSmallest}");
+Console.WriteLine($"The average is {average}");
