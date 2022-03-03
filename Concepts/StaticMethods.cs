a836b1f7dc148471d644e05c0d52991c82ab23ea
jamesa <james.aslett@directlife.co.uk>
2022-03-03 13:57:56 +0000

    Added StaticMethods

---
+++ Concepts/StaticMethods.cs
@@ -0,0 +1,19 @@
+﻿//In the below example, you can access the text string (the scope of which is the Main method) within the DisplayText method
+
+string text = Console.ReadLine();
+
+DisplayText();
+
+void DisplayText()
+{
+    Console.WriteLine(text);
+}
+
+//but in this example, we've added static to the method. Static prevents you from using variables within the containing method. It can be used as a safety precaution
+
+DisplayText2();
+
+static void DisplayText2()
+{
+    Console.WriteLine(text);
+}
