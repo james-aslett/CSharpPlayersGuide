d2b3b8277d06376bf4a770340b5b93e747c1416f
James Aslett <JamesA@directlife.co.uk>
2022-03-07 00:00:37 +0000

    Completed Countdown

---
+++ Challenges/Countdown.cs
@@ -0,0 +1,12 @@
+﻿int Countdown(int number)
+{
+    if (number == 1)
+    {
+        Console.WriteLine(number);
+        return 1;
+    }
+    Console.WriteLine(number);
+    return number - Countdown(number - 1);
+}
+
+Countdown(10);
--- Program.cs
+++ Program.cs
@@ -1,7 +1,1 @@
-﻿int Factorial(int number)
-{
-    if (number == 1) return 1;
-    return number * Factorial(number - 1);
-}
-
-Factorial(5);
+﻿
