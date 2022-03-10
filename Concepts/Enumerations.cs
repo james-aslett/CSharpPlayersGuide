96e7b4844e2304ae12983119e12dbc3f0bdc8caf
jamesa <james.aslett@directlife.co.uk>
2022-03-10 14:31:30 +0000

    Started SimulasTest

---
+++ Concepts/Enumerations.cs
@@ -0,0 +1,11 @@
+﻿Season current = Season.Summer;
+
+if (current == Season.Summer || current == Season.Winter)
+    Console.WriteLine("Happy, solstice!");
+else
+    Console.WriteLine("Happy equinox!");
+
+//enums MUST go after any other code, or in a separate file
+
+//the first item is the default
+enum Season { Winter, Spring, Summer, Fall }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿ChestState currentState = ChestState.Locked;
+
+
+
+
+//infinite loop
+Console.WriteLine($"The chest is {ChestState.Locked}. What do you want to do?");
+string userResponse = Console.ReadLine();
+SetChestState(userResponse);
+
+
+void SetChestState(string response)
+{
+    if (response == "lock") currentState = ChestState.Locked;
+    else if (response == "unlock") currentState = ChestState.Unlocked;
+    else if (response == "open") currentState = ChestState.Open;
+    else currentState = ChestState.Closed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+enum ChestState { Locked, Unlocked, Open, Closed }
