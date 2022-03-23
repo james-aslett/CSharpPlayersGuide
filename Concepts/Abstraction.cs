656c790d5ae476e396435c3dfcffa7c4bc1d980e
jamesa <james.aslett@directlife.co.uk>
2022-03-23 13:15:18 +0000

    Updates

---
+++ Concepts/Abstraction.cs
@@ -0,0 +1,41 @@
+﻿//Earlier versions of our Rectangle class (see Information Hiding.cs) had a field for the rectangle's area, which got updated every time the width or height changed.
+//But we can change this to compute the area as needed and ditch the field without affecting the rest of our program
+
+class Rectangle
+{
+    private float _width;
+    private float _height;
+    //private float _area //we've removed this
+
+    public Rectangle(float width, float height)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    public float GetWidth() => _width;
+    public float GetHeight() => _height;
+
+    //area now calculated on demand. The outside world is oblivious to this change. They used to retrieve the area through GetArea() and they still do
+    public float GetArea() => _width * _height;
+
+    public void SetWidth(float value) => _width = value;
+    public void SetHeight(float value) => _height = value;
+}
+
+//difference between public and internal
+
+//public can be accessed anywhere, including in other projects
+public class Rectangle2
+{
+
+}
+
+//internal can only be used in the project in which it is defined
+internal class Rectangle3
+{
+
+}
+
+//if you make a class and feel that its role is a supporting role - details that help other classes accomplish their job - but not something you want the
+//outside world to know about, then internal could be a suitable choice
--- Concepts/InformationHiding.cs
+++ Concepts/InformationHiding.cs
@@ -22,7 +22,7 @@ class Rectangle
     {
         _width = width;
         _height = height;
-        _area = width * height;
+        _area = UpdateArea(_width, _width);
     }
 
     //these public methods allow the outside world to access the data behind the private fields without having direct access to them
@@ -34,13 +34,20 @@ class Rectangle
     public void SetWidth(float value)
     {
         _width = value;
-        _area = _width * _height;
+        _area = UpdateArea(_width, _width);
     }
 
     public void SetHeight(float value)
     {
         _height = value;
-        _area = _width * _height;
+        _area = UpdateArea(_width, _width);
+    }
+
+    //updating the area is not something the outside world should have to request specifically. It is details of how we have created the Rectangle class, so
+    //is best off as a private method
+    private float UpdateArea(float width, float height)
+    {
+        return width * height;
     }
 
     //we've decided that it is reasonable to ask a rectangle to update its width and height and so have added methods for these. But we've decided not to let people
@@ -49,5 +56,9 @@ class Rectangle
     //methods that retriee a field's current value are called getters
     //methods that assign a new value to a field are called setters
 
-}
+    //information hiding allows an object to protect its data. Each object is its own gatekeeper. If another object wants to see what state the object is in
+    //or change its state, it must request that information by calling a getter or a setter method, rather than just grabbing it directly. This way, objects
+    //can enforce rules about their data
 
+    //information hiding comes at a cost of added complexity, but it's worth it, and we'll see a better better example in Level 20
+}
