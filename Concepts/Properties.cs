1414fddb3b663ec325040f7e8ffccfd33763796e
jamesa <james.aslett@directlife.co.uk>
2022-03-28 12:38:21 +0100

    Started Properties

---
+++ Concepts/Properties.cs
@@ -0,0 +1,34 @@
+﻿class MyClass
+{
+    private float _width;
+    public float _height;
+
+    //properties automatically include a specicial 'value' variable, so we don't need to define one
+    public float WidthExpressionBody
+    {
+        get => _width;
+        set => _width = value;
+    }
+
+    public float WidthBlockBody
+    {
+        get
+        {
+            return _width;
+        }
+        set
+        {
+            _width = value;
+        }
+    }
+
+    //get-only properties make sense for something that can't be changed from the outside
+    public float Area
+    {
+        get => _width * _height;
+    }
+
+    //if the property is get-only with an expression body like above, we can simplify it further
+    public float AreaSimple => _width * _height;
+
+}
