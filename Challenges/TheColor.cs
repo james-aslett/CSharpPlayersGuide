3739844e76951b9ac43381e617aa0d5948c06813
jamesa <james.aslett@directlife.co.uk>
2022-04-08 13:35:43 +0100

    Completed ThePoint | Started TheColor

---
+++ Challenges/TheColor.cs
@@ -0,0 +1,6 @@
+﻿//define a new Color class
+//define properties for red, green and blue channels
+//add appropriate constructors that you feel make sense for creating new Color objects
+//create static properties to define the eight commonly used colors for easy access
+//in Main, make two Color-typed variables. Use a constructor to make a color instance and use a static property for the other two. Display each of their
+//red, green and blue channel values
---
+++ Challenges/ThePoint.cs
@@ -0,0 +1,31 @@
+﻿//in Main, create a point at (2,3) and another at (-4,0) and display them in the console with the format (x, y)
+Point point1 = new(2,3);
+Point point2 = new(-4, 0);
+Point point3 = new();
+
+Console.WriteLine($"The first point is ({point1.X},{point1.Y})");
+Console.WriteLine($"The second point is ({point2.X},{point2.Y})");
+Console.WriteLine($"The third point is ({point3.X},{point3.Y})");
+
+//define Point class
+public class Point
+{
+    //define X/Y properties
+    public float X { get; }
+    public float Y { get; }
+
+    //constructor to create a point from a specific X/Y coordinate
+    public Point(float x, float y) 
+    {
+        X = x;
+        Y = y;
+    }
+
+    //parameterless constructor to create a point at (0,0). Has an empty body
+    public Point() : this(0, 0) { }
+}
+
+//Answer this question: are your X and Y properties immutable? Why did you choose what you did?
+
+//They are immutable because they have no ability to set a new value for either one (they have no setters) - usually best to prefer immutable unless
+//there's a reason to make them mutable
