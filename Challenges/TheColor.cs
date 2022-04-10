e287236c8c0e82f5c2eba471e6bb4a382cb346ee
James Aslett <JamesA@directlife.co.uk>
2022-04-10 21:58:32 +0100

    Update

--- Challenges/TheColor.cs
+++ Challenges/TheColor.cs
@@ -1,5 +1,16 @@
 ﻿//define a new Color class
-//define properties for red, green and blue channels
+public class Color
+{
+    //define properties for red, green and blue channels
+    //red #FF0000
+    //green #00FF00
+    //blue #0000FF
+
+    public string Red { get; }
+    public string Green { get; }
+    public string Blue { get; }
+
+}
 //add appropriate constructors that you feel make sense for creating new Color objects
 //create static properties to define the eight commonly used colors for easy access
 //in Main, make two Color-typed variables. Use a constructor to make a color instance and use a static property for the other two. Display each of their
