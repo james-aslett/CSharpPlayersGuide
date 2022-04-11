bad1012069d44c2ae3def8bc227c66485f6d9b83
jamesa <james.aslett@directlife.co.uk>
2022-04-11 13:10:06 +0100

    Finished TheColor

--- Challenges/TheColor.cs
+++ Challenges/TheColor.cs
@@ -1,16 +1,33 @@
-﻿//define a new Color class
+﻿Color babyPink = new(255, 204, 229);
+Color specificColor = Color.White;
+Console.WriteLine($"My babyPink color has the following values: R {babyPink.R} | G {babyPink.G} | B {babyPink.B}");
+Console.WriteLine($"My specific color has the following values: R {specificColor.R} | G {specificColor.G} | B {specificColor.B}");
+
 public class Color
 {
-    //define properties for red, green and blue channels
-    //red #FF0000
-    //green #00FF00
-    //blue #0000FF
+    //properties for R/G/B
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
 
-    public string Red { get; }
-    public string Green { get; }
-    public string Blue { get; }
+    //Constructor to create a color
+    public Color(byte r, byte g, byte b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
 
+    //properties for custom color, of type Color (new Color(int, int, int))
+    public static Color White { get; } = new Color(255, 255, 255);
+    public static Color Black { get; } = new Color(0, 0, 0);
+    public static Color Red { get; } = new Color(255, 0, 0);
+    public static Color Orange { get; } = new Color(255, 165, 0);
+    public static Color Yellow { get; } = new Color(255, 255, 0);
+    public static Color Green { get; } = new Color(0, 128, 0);
+    public static Color Blue { get; } = new Color(0, 0, 255);
+    public static Color Purple { get; } = new Color(128, 0, 128);
 }
-//add appropriate constructors that you feel make sense for creating new Color objects
-//create static properties to define the eight commonly used colors for easy access
-//in Main, make two Color-typed variables. Use a constructor to make a color instance and use a static property for the other two. Display each of their
+
+
+
