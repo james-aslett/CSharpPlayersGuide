72a25eb8f046faf38da2d21427b5026a3eace478
James Aslett <JamesA@directlife.co.uk>
2022-04-11 22:24:07 +0100

    Updated TheCard

--- Challenges/TheCard.cs
+++ Challenges/TheCard.cs
@@ -1,31 +1,36 @@
-﻿//Create a Main method that will create a card instance for the whole deck (every color with every rank) and display each (for example,
-//"The Red Ampersand" and "The Blue Seven"
+﻿class Program
+{
+    static void Main()
+    {
+        foreach (var color in Enum.GetNames(typeof(CardColor)))
+        {
+            Console.WriteLine(color);
+        }
 
+        foreach (var rank in Enum.GetNames(typeof(CardRank)))
+        {
+            Console.WriteLine(rank);
+        }
 
-
-//Create a card to represent playing cards. Each card has:
-//    a color (red, green, blue, yellow)
-//    a rank (1 - 10, followed by the symbols $, %, ^, and &)
+        Console.WriteLine($"The {CardColor.Red} {CardRank.Ampersand}");
+    }
+}
 
 //Define a Card class to represent a card with a color and a rank, as described above
 public class Card
 {
-    public Color {get;}
+    //Add properties or methods that tell you if a card is a number or a symbol card (the equivalent of a face card)
+    public string Color { get; }
+    public string Rank { get; }
 
     public Card(string color, string rank)
     {
-
+        Color = color;
+        Rank = rank;
     }
- 
-    //Add properties or methods that tell you if a card is a number or a symbol card (the equivalent of a face card)
-
-
-
 }
 
-//Define enumerations for card colors and ranks
 public enum CardColor { Red, Green, Blue, Yellow }
-public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand
-}
+public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand }
 
 //Answer this question: "Why do you think we used a color enumeration here but made a color class in the previous challenge?
