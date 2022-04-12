36cc230b902b1115d4754f64722945edba1ad064
jamesa <james.aslett@directlife.co.uk>
2022-04-12 13:40:07 +0100

    Updates

--- Challenges/TheCard.cs
+++ Challenges/TheCard.cs
@@ -1,36 +1,39 @@
-﻿class Program
+﻿//create new var of type Color enum as array
+Color[] colors = new Color[] { Color.Red, Color.Green, Color.Blue, Color.Yellow };
+//create new var of type Rank enum as array
+Rank[] ranks = new Rank[] { Rank.One, Rank.Two, Rank.Three, Rank.Four, Rank.Five, Rank.Six, Rank.Seven, Rank.Eight, Rank.Nine, Rank.Ten, Rank.Dollar, Rank.Percent, Rank.Caret, Rank.Ampersand };
+
+//nested foreach loop to iterate over each color and each rank and diplay the combination to console
+foreach (Color color in colors)
 {
-    static void Main()
+    foreach (Rank rank in ranks)
     {
-        foreach (var color in Enum.GetNames(typeof(CardColor)))
-        {
-            Console.WriteLine(color);
-        }
-
-        foreach (var rank in Enum.GetNames(typeof(CardRank)))
-        {
-            Console.WriteLine(rank);
-        }
-
-        Console.WriteLine($"The {CardColor.Red} {CardRank.Ampersand}");
+        //create new var of type Card which takes Color/Rank as parameters (see constructor below)
+        Card card = new(color, rank);
+        Console.WriteLine($"The {card.Color} {card.Rank}");
     }
 }
 
-//Define a Card class to represent a card with a color and a rank, as described above
 public class Card
 {
-    //Add properties or methods that tell you if a card is a number or a symbol card (the equivalent of a face card)
-    public string Color { get; }
-    public string Rank { get; }
+    //property of type Color (enum)
+    public Color Color { get; }
+    //property of type Rank (enum)
+    public Rank Rank { get; }
 
-    public Card(string color, string rank)
+    //constructor takes a Color/Rank enums as parameters
+    public Card(Color color, Rank rank)
     {
+        //set above properties to parmeter inputs
         Color = color;
         Rank = rank;
     }
+
+    public bool IsSymbol => Rank == Rank.Ampersand || Rank == Rank.Caret || Rank == Rank.Dollar || Rank == Rank.Percent;
+    public bool IsNumber => !IsSymbol;
 }
 
-public enum CardColor { Red, Green, Blue, Yellow }
-public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand }
+public enum Color { Red, Green, Blue, Yellow }
+public enum Rank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand }
 
 //Answer this question: "Why do you think we used a color enumeration here but made a color class in the previous challenge?
