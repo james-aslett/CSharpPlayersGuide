4fc95b28d663b04bc6e62d6beae6819d392819a7
jamesa <james.aslett@directlife.co.uk>
2022-04-12 13:40:07 +0100

    Completed TheCard | Started TheLockedDoor

---
+++ Challenges/TheLockedDoor.cs
@@ -0,0 +1,20 @@
+﻿//SimulasTest might help!
+
+//DOOR RULES:
+//1: An open door can be closed
+//2: A closed (but not locked) door can be opened
+//3: A closed door can be locked
+//4: A locked door can be unlocked, but numeric password is needed and will only unlock if the code provided matches the door's current passcode
+//5: When a door is created, it must be given an initial passcode
+//6: You should be able to change the passcode by supplying the current code and a new one. It should only change if the correct current code is given
+
+//OBJECTIVES
+//Define a Door class that can keep track of whether it is locked, open or closed
+//Create methods to perform the four transitions defined above
+//Build a constructor that requires the starting numeric passcode
+//Build a method that will allow you to change the passcode for an existing door by supplying the current passcode and a new passcode. Only change the passcode is
+//the current one is correct
+//Make your Main method ask the user for a starting passcode, then create a new Door instance. Allow the user to attemp the four transitions (open, close, lock, unlock)
+//and change the code by typing in text commands
+
+
