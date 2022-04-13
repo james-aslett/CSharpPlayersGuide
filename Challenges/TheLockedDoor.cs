bc40e2bee9e7b31dd6c6acde59772d8d5e08ffd1
James Aslett <JamesA@directlife.co.uk>
2022-04-13 22:28:47 +0100

    Updates

--- Challenges/TheLockedDoor.cs
+++ Challenges/TheLockedDoor.cs
@@ -6,14 +6,38 @@
 //The door is Closed. What do you want to do? (open, close, lock, unlock, change code) change code
 //What is the current passcode? 1234 (if matches then next time you unlock it'll change to Closed, if doesn't match then next time you unlock it'll stay on Locked)
 
+int initialPasscode = GetInt("What is the initial passcode?");
+Door door = new(initialPasscode);
+
+while (true)
+{
+    Console.Write($"The door is {door.State.ToString().ToLower()}. What do you want to do? (open, close, lock, unlock, change code) ");
+    string? command = Console.ReadLine();
+
+}
+
+int GetInt(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
 public class Door
 {
-    DoorState ManipulateDoor(string input)
+    public int _passcode;
+    public DoorState _doorstate;
+
+    public Door(int passcode)
     {
-        if (input == "close" || input == "unlock") return DoorState.Closed;
-        else if (input == "lock") return DoorState.Locked;
-        else return DoorState.Open;
+        _passcode = passcode;
+    }
+    public DoorState ManipulateDoor(string input)
+    {
+        if (input == "close" || input == "unlock") _doorstate = DoorState.Closed;
+        else if (input == "lock") _doorstate = DoorState.Locked;
+        else _doorstate = DoorState.Open;
     }
 }
 
-enum DoorState { Open, Closed, Locked }
+
+public enum DoorState { Open, Closed, Locked }
