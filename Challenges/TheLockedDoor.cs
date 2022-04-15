e3e38668a5f686a01ee4783efd61c8ce54f0fdb5
James Aslett <JamesA@directlife.co.uk>
2022-04-15 22:16:31 +0100

    TheLockedDoor complete

--- Challenges/TheLockedDoor.cs
+++ Challenges/TheLockedDoor.cs
@@ -1,12 +1,4 @@
-﻿//PROGRAM
-//What is the initial passcode? 1234
-//The door is Closed. What do you want to do? (open, close, lock, unlock, change code) lock
-//The door is Locked.What do you want to do? (open, close, lock, unlock, change code) unlock
-//What is the passcode? - if matches then 'The door is Closed...', if not match then 'The door is Locked...'
-//The door is Closed. What do you want to do? (open, close, lock, unlock, change code) change code
-//What is the current passcode? 1234 (if matches then next time you unlock it'll change to Closed, if doesn't match then next time you unlock it'll stay on Locked)
-
-int initialPasscode = GetInt("What is the initial passcode?");
+﻿int initialPasscode = GetInt("What is the initial passcode?");
 Door door = new Door(initialPasscode);
 
 while (true)
