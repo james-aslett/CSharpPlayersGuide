d99e9cc0d0623f24e8b4813cd41d0cb5ad1841a0
James Aslett <JamesA@directlife.co.uk>
2022-04-24 21:24:38 +0100

    Completed ThePasswordValidator

--- Challenges/ThePasswordValidator.cs
+++ Challenges/ThePasswordValidator.cs
@@ -2,65 +2,91 @@
 //Define a PasswordValidator class that can be given a password and validate it
 //Make Main method loop forever, asking for a password and reporting whether the password is allowed using an instance of the PasswordValidator class
 
+PasswordValidator validator = new PasswordValidator();
+
 while (true)
 {
-    Console.WriteLine("Please enter a password between 6-13 characters. Must not contain 'T' or '&' and must contain one upper character, lower character and number");
-    PasswordValidator pw = new PasswordValidator(Console.ReadLine());
-    if (pw.IsPasswordValid == false)
+    Console.Write("Please enter a password between 6-13 characters. Must not contain 'T' or '&' and must contain one upper character, lower character and number. ");
+    string? password = Console.ReadLine();
+
+    if (password == null) break; // If the user enters a null password (Ctrl+Z) then let's be done.
+                                 // An alternative could be to make 'IsValid' handle null or to fall
+                                 // back to some default string like the empty string ("") instead.
+                                 // This challenge doesn't specifically call out dealing with null,
+                                 // and it isn't easy to get a null in there in the first place. If
+                                 // you ignored this possibility, that's fine too.
+
+    if (validator.IsValid(password))
     {
-        Console.WriteLine("Sorry, that password is invalid. Try again.");
+        Console.WriteLine("Lovely job, that password is accepted! ");
     }
     else
     {
-        Console.WriteLine("Lovely job, that password is accepted!");
+        Console.WriteLine("Sorry, that password is invalid. Try again. ");
     }
 }
 
 public class PasswordValidator
 {
-    private string _password;
-    public bool IsPasswordValid = false;
-
-    public PasswordValidator(string password)
+    public bool IsValid(string password)
     {
-        _password = password;
-        ValidatePassword(password);
+        if (password.Length < 6) return false;
+        if (password.Length > 13) return false;
+        if (!ContainsUpper(password)) return false;
+        if (!ContainsLower(password)) return false;
+        if (!ContainsDigit(password)) return false;
+        if (Contains(password, 'T')) return false;
+        if (Contains(password, '&')) return false;
+
+        return true;
+
     }
 
-    public string? GetString(string input)
+    private static bool ContainsUpper(string password)
     {
-        Console.WriteLine($"{input} ");
-        return Console.ReadLine();
+        foreach (char letter in password)
+        {
+            if (char.IsUpper(letter))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public bool ValidatePassword(string password)
+    private static bool ContainsLower(string password)
     {
-        int allowedChars = 0;
-
-        if (password.Length < 6 | password.Length > 13)
+        foreach (char letter in password)
         {
-            return IsPasswordValid;
-        }
-
-        foreach (char c in password)
-        {
-            if (char.IsUpper(c) = 'T' | char.IsSymbol(c) = '&')
+            if (char.IsLower(letter))
             {
-                return IsPasswordValid;
-            }
-            else if (char.IsUpper(c) | char.IsLower(c) | char.IsDigit(c))
-            {
-                allowedChars++;
+                return true;
             }
         }
+        return false;
+    }
 
-        if (allowedChars == 3)
+    private static bool ContainsDigit(string password)
+    {
+        foreach (char letter in password)
         {
-            return IsPasswordValid = true;
+            if (char.IsDigit(letter))
+            {
+                return true;
+            }
         }
-        else
+        return false;
+    }
+
+    private static bool Contains(string password, char input)
+    {
+        foreach (char letter in password)
         {
-            return IsPasswordValid;
+            if (letter == input)
+            {
+                return true;
+            }
         }
+        return false;
     }
 }
