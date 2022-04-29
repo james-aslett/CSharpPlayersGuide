d41f24c5aef3a500fdd433eae950ebe8a68bf691
jamesa <james.aslett@directlife.co.uk>
2022-04-29 13:39:46 +0100

    Started 15-Puzzle

---
+++ Challenges/15-Puzzle.cs
@@ -0,0 +1,11 @@
+﻿//15-Puzzle contains a set of numbered tiles on a board with a single open slot. The aim is to rearrange the tiles to put the numbers in order with the open slot 
+//at the bottom right.
+//The player needs to be able to manipulate the board to rearrange it
+//The current state of the game needs to be displayed to the user
+//The game needs to be displayed to the user
+//The game needs to be able to generate random puzzles to solve
+//The game needs to track and display how many moves the player has made
+
+//OBJECTIVES:
+//Use CRC Cards to outline the objects and classes that may be needed to make the game of 15-Puzzle
+//Answer this question: Would your design need to change if we also wanted 3x3 or 5x5 boards?
