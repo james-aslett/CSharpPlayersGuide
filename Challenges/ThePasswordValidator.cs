fd9a27ba468dedd5a7707d8d1d44a348453b6769
jamesa <james.aslett@directlife.co.uk>
2022-05-10 13:45:19 +0100

    Refactored ThePasswordValidator

--- Challenges/ThePasswordValidator.cs
+++ Challenges/ThePasswordValidator.cs
@@ -1,91 +1,51 @@
-﻿//STEPS:
-//Define a PasswordValidator class that can be given a password and validate it
-//Make Main method loop forever, asking for a password and reporting whether the password is allowed using an instance of the PasswordValidator class
-
-PasswordValidator validator = new PasswordValidator();
+﻿PasswordValidator validator = new PasswordValidator();
 
 while (true)
 {
     Console.Write("Please enter a password between 6-13 characters. Must not contain 'T' or '&' and must contain one upper character, lower character and number. ");
     string? password = Console.ReadLine();
-
-    if (password == null) break; // If the user enters a null password (Ctrl+Z) then let's be done.
-                                 // An alternative could be to make 'IsValid' handle null or to fall
-                                 // back to some default string like the empty string ("") instead.
-                                 // This challenge doesn't specifically call out dealing with null,
-                                 // and it isn't easy to get a null in there in the first place. If
-                                 // you ignored this possibility, that's fine too.
-
-    if (validator.IsValid(password))
-    {
-        Console.WriteLine("Lovely job, that password is accepted! ");
-    }
-    else
-    {
-        Console.WriteLine("Sorry, that password is invalid. Try again. ");
-    }
+    if (string.IsNullOrEmpty(password)) break; // If the user enters a null/empty password (Ctrl+Z) then end program
+    ValidationResult result = validator.IsValid(password);
+    if (result == ValidationResult.Success) Console.WriteLine("Great! Password accepted!");
+    else if (result == ValidationResult.TooShort) Console.WriteLine("Password is too short. Must be at least 6 characters.");
+    else if (result == ValidationResult.TooLong) Console.WriteLine("Password is too long. Must be 13 characters maximum.");
+    else if (result == ValidationResult.NeedsUpperCase) Console.WriteLine("Password must contain an upper case letter.");
+    else if (result == ValidationResult.NeedsLowerCase) Console.WriteLine("Password must contain a lower case letter.");
+    else if (result == ValidationResult.NeedsNumber) Console.WriteLine("Password must contain a number.");
+    else if (result == ValidationResult.ContainsUpperCaseT) Console.WriteLine("Password cannot contain 'T'.");
+    else if (result == ValidationResult.ContainsAmpersand) Console.WriteLine("Password cannot contain '&'.");
 }
 
 public class PasswordValidator
 {
-    public bool IsValid(string password)
+    public ValidationResult IsValid(string password)
     {
-        if (password.Length < 6) return false;
-        if (password.Length > 13) return false;
-        if (!ContainsUpper(password)) return false;
-        if (!ContainsLower(password)) return false;
-        if (!ContainsDigit(password)) return false;
-        if (Contains(password, 'T')) return false;
-        if (Contains(password, '&')) return false;
+        if (password.Length < 6) return ValidationResult.TooShort;
+        if (password.Length > 13) return ValidationResult.TooLong;
 
-        return true;
-    }
+        int uppercaseCount = 0;
+        int lowercaseCount = 0;
+        int numberCount = 0;
+        int capitalTCount = 0;
+        int ampersandCount = 0;
 
-    private static bool ContainsUpper(string password)
-    {
         foreach (char letter in password)
         {
-            if (char.IsUpper(letter))
-            {
-                return true;
-            }
+            if (char.IsUpper(letter)) uppercaseCount++;
+            if (char.IsLower(letter)) lowercaseCount++;
+            if (char.IsDigit(letter)) numberCount++;
+            if (letter == 'T') capitalTCount++;
+            if (letter == '&') ampersandCount++;
         }
-        return false;
-    }
 
-    private static bool ContainsLower(string password)
-    {
-        foreach (char letter in password)
-        {
-            if (char.IsLower(letter))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+        if (uppercaseCount == 0) return ValidationResult.NeedsUpperCase;
+        if (lowercaseCount == 0) return ValidationResult.NeedsLowerCase;
+        if (numberCount == 0) return ValidationResult.NeedsNumber;
+        if (capitalTCount > 0) return ValidationResult.ContainsUpperCaseT;
+        if (ampersandCount > 0) return ValidationResult.ContainsAmpersand;
 
-    private static bool ContainsDigit(string password)
-    {
-        foreach (char letter in password)
-        {
-            if (char.IsDigit(letter))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static bool Contains(string password, char input)
-    {
-        foreach (char letter in password)
-        {
-            if (letter == input)
-            {
-                return true;
-            }
-        }
-        return false;
+        return ValidationResult.Success;
     }
 }
+
+public enum ValidationResult { Success, TooShort, TooLong, NeedsUpperCase, NeedsLowerCase, NeedsNumber, ContainsUpperCaseT, ContainsAmpersand }
