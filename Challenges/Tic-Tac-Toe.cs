80d17e96f27152c8cc0183f6f6aeb189f6d1ccf9
jamesa <james.aslett@directlife.co.uk>
2022-06-14 13:03:17 +0100

    Updated Tic-Tac-Toe

--- Challenges/Tic-Tac-Toe.cs
+++ Challenges/Tic-Tac-Toe.cs
@@ -1,35 +1,89 @@
-﻿Board board = new Board();
-board.Set(new Location(0, 1), CellType.O);
-board.Set(new Location(0, 2), CellType.X);
-board.Set(new Location(2, 2), CellType.X);
-board.Set(new Location(2, 1), CellType.O);
-new BoardRenderer().Render(board);
-new Player().GetChoice(board);
+﻿new TicTacToeGame().Run();
+
+public class WinChecker
+{
+    public bool HasXWon(Board board)
+    {
+        //columns
+        if (board.Get(0, 0) == CellType.X && board.Get(0, 1) == CellType.X && board.Get(0, 2) == CellType.X) return true;
+        if (board.Get(1, 0) == CellType.X && board.Get(1, 1) == CellType.X && board.Get(1, 2) == CellType.X) return true;
+        if (board.Get(2, 0) == CellType.X && board.Get(2, 1) == CellType.X && board.Get(2, 2) == CellType.X) return true;
+
+        //rows
+        if (board.Get(0, 0) == CellType.X && board.Get(1, 0) == CellType.X && board.Get(2, 0) == CellType.X) return true;
+        if (board.Get(0, 1) == CellType.X && board.Get(1, 1) == CellType.X && board.Get(2, 1) == CellType.X) return true;
+        if (board.Get(0, 2) == CellType.X && board.Get(1, 2) == CellType.X && board.Get(2, 2) == CellType.X) return true;
+
+        //diagonals
+        if (board.Get(0, 0) == CellType.X && board.Get(1, 1) == CellType.X && board.Get(2, 2) == CellType.X) return true;
+        if (board.Get(2, 0) == CellType.X && board.Get(1, 1) == CellType.X && board.Get(0, 2) == CellType.X) return true;
+
+        return false;
+    }
+
+    public bool HasOWon(Board board)
+    {
+        return false;
+    }
+
+    public bool IsBoardFull(Board board)
+    {
+        for (int row = 0; row < 3; row++)
+            for (int column = 0; column < 3; column++)
+                if (board.Get(new Location(row, column)) == CellType.Empty)
+                    return false;
+        return true;
+    }
+}
+
+public class TicTacToeGame
+{
+    public void Run()
+    {
+        Board board = new Board();
+        BoardRenderer renderer = new BoardRenderer();
+        Player player1 = new Player(CellType.X);
+        Player player2 = new Player(CellType.O);
+        Player currentPlayer = player1;
+        WinChecker winchecker = new WinChecker();
+
+        while (!winchecker.HasXWon(board) && !winchecker.HasOWon(board) && !winchecker.IsBoardFull(board))
+        {
+            renderer.Render(board);
+            Location target = currentPlayer.GetChoice(board);
+            board.Set(target, currentPlayer.Token);
+
+            if (currentPlayer == player1) currentPlayer = player2;
+            else currentPlayer = player1;
+        }
+    }
+}
 
 public class Player
 {
+    public CellType Token { get; }
+    public Player(CellType token) { Token = token; }
     public Location GetChoice(Board board)
     {
         while (true)
         {
             Console.Write("What square do you want to play in? ");
             string? choiceAsString = Console.ReadLine();
-            int choice;
 
             //will refactor this with proper error handling later
-            if (!int.TryParse(choiceAsString, out choice))
+            if (!int.TryParse(choiceAsString, out int choice))
             {
                 Console.WriteLine("That was not a valid choice. Try again.");
                 continue;
             }
 
             Location? location = ToLocation(choice);
+
             if (location == null)
             {
                 Console.WriteLine("That was not a valid choice. Try again.");
                 continue;
             }
-
             if (board.Get(location) == CellType.Empty)
                 return location;
             Console.WriteLine("That space is not open! Try again.");
@@ -98,6 +152,11 @@ public class Board
     public CellType Get(Location location)
     {
         return _cells[location.Row, location.Column];
+    }
+
+    public CellType Get(int row, int column)
+    {
+        return _cells[row, column];
     }
 }
 
