fcb18bdb1d0460ea8af073253095b01a7dc4140b
James Aslett <JamesA@directlife.co.uk>
2022-06-27 22:15:10 +0100

    PackingInventory update

--- Challenges/PackingInventory.cs
+++ Challenges/PackingInventory.cs
@@ -8,13 +8,37 @@ Pack pack = new Pack(10, 20, 30);
 
 while (true)
 {
+    Console.WriteLine($"Pack is currently at {pack.CurrentCount}/{pack.MaxCount} items, {pack.CurrentWeight}/{pack.MaxWeight} weight, and {pack.CurrentVolume}/{pack.MaxVolume} volume.");
 
+    Console.WriteLine("What do you want to add?");
+    Console.WriteLine("1 - Arrow");
+    Console.WriteLine("2 - Bow");
+    Console.WriteLine("3 - Rope");
+    Console.WriteLine("4 - Water");
+    Console.WriteLine("5 - Food");
+    Console.WriteLine("6 - Sword");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    InventoryItem newItem = choice switch
+    {
+        1 => new Arrow(),
+        2 => new Bow(),
+        3 => new Rope(),
+        4 => new Water(),
+        5 => new Food(),
+        6 => new Sword()
+    };
+
+    if (!pack.Add(newItem))
+        Console.WriteLine("Could not add this to the pack.");
 }
 
 //Build a 'Pack' class that can store an array of items. The total number of items, the maximum weight, and the maximum volume are provived at creation time and
 //cannot change afterwards
 public class Pack
 {
+    //Add properties to Pack that allow it to report the current item count, weight, and volume, and the limits of each
+
     public int MaxCount { get; }
     public float MaxWeight { get; }
     public float MaxVolume { get; }
@@ -34,6 +58,8 @@ public class Pack
         _items = new InventoryItem[maxCount];
     }
 
+    //Make a 'public bool Add(InventoryItem item)' method to 'Pack' that allows you to add items of any type to the pack's contents. This method should fail (return
+    //false and not modify the pack's fields) if adding an item would cause it to exceed the pack's item, weight or volume limit
     public bool Add(InventoryItem item)
     {
         if (CurrentCount >= MaxCount) return false;
@@ -47,12 +73,6 @@ public class Pack
         return true;
     }
 }
-
-//Make a 'public bool Add(InventoryItem item)' method to 'Pack' that allows you to add items of any type to the pack's contents. This method should fail (return
-//false and not modify the pack's fields) if adding an item would cause it to exceed the pack's item, weight or volume limit
-
-//Add properties to Pack that allow it to report the current item count, weight, and volume, and the limits of each
-
 
 
 //Create an inventory item class that represents any of the different item types. This class must represent the item's weight and volume, which it needs
@@ -75,7 +95,7 @@ public class Arrow : InventoryItem { public Arrow() : base(0.1f, 0.05f) { } }
 public class Bow : InventoryItem { public Bow() : base(1f, 4f) { } }
 public class Rope : InventoryItem { public Rope() : base(1f, 1.5f) { } }
 public class Water : InventoryItem { public Water() : base(2f, 3f) { } }
-public class FoodRations : InventoryItem { public FoodRations() : base(1f, 0.5f) { } }
+public class Food : InventoryItem { public Food() : base(1f, 0.5f) { } }
 public class Sword : InventoryItem { public Sword() : base(5f, 3f) { } }
 
 
