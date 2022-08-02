0c923af293ea7a9f45933145e7d02d5b4d3b5991
jamesa <james.aslett@directlife.co.uk>
2022-08-02 17:08:05 +0100

    Updates

--- Concepts/Interfaces.cs
+++ Concepts/Interfaces.cs
@@ -15,7 +15,7 @@
 //Let's say we have a game where the player advances through the levels one at a time. Each level is a grid of different terrain types:
 public enum TerrainType { Desert, Forests, Mountains, Pastures, Fields, Hills }
 
-//Each level is a 2D grid of these terrain types, represented by an instance of this class:
+//Each level is a 2D grid of terrain types, represented by an instance of this class:
 public class Level
 {
     public int Width { get; }
@@ -23,7 +23,7 @@ public class Level
     public TerrainType GetTerrainAt(int row, int column) { /* ... */ }
 }
 
-//We find a user for interfaces when deciding where level definitions come from. There are many options. We could define them directly in code,
+//We find a use for interfaces when deciding where level definitions come from. There are many options. We could define them directly in code,
 //setting terrain types at each row and column in C# code. We could load them from files on our computer. We could load them from a database.
 //There are many options, yet each one has the same responsibility - create a level. Yet, the code for each is entirely unrelated to the code for
 //the other options.
@@ -51,7 +51,7 @@ public interface ILevelBuilder
 //Once an interface has been created, the next step is to build a class that fulfills the job described by the interface. This is called implementing
 //the interface. It looks like inheritance, so some devs also call is extending/deriving from the interface.
 
-//The simplest implementaton of the ILevelBuilder interfae is probably defining levels in code:
+//The simplest implementaton of the ILevelBuilder interface is probably defining levels in code:
 public class FixedLevelBuilder : ILevelBuilder
 {
     public Level BuildLevel(int levelNumber)
@@ -64,7 +64,7 @@ public class FixedLevelBuilder : ILevelBuilder
     }
 }
 
-//You must define each member included in the interface, as we did with the BuildLevel method. These will be public but do not put the ovveride keyword
+//You must define each member included in the interface, as we did with the BuildLevel method. These will be public but do not put the override keyword
 //on them. This isn't an override. It is simply filling in the definition of how this class performs the job it has claimed to do by implementing
 //the interface.
 
@@ -87,28 +87,143 @@ while (true)
 //The rest of the game doesn't care which implementation of ILevelBuilder is being used. However, with the code we have written so far, we know it will
 //be FixedLevelBuilder since that is the only one that exists. However, by doing nothing more than adding a new class that implements ILevelBuilder
 //and changing the implementation of LocateLevelBuilder to return that instead, we can completely change the source of levels in our game. The entire
-//rest of the game does not care where they come from, as long as the object building them confirms to the ILevelBuilder interface. We have reserved
+//rest of the game does not care where they come from, as long as the object building them conforms to the ILevelBuilder interface. We have reserved
 //a great degree of flexibility for the future by merely defining and using an interface.
 
 //INTERFACES AND BASE CLASSES
 
 //Interfaces and base classes can play nicely together. A class can simultaneously extend a base class and implement an interface. Do this by listing
 //the base class followed by the interface after the colon, separated by commas:
-public class MySqlDatabaseLevelBuilder : BasicDatabaseLevelBuilder : ILevelBuilder { /*...*/ }
+public class MySqlDatabaseLevelBuilder : BasicDatabaseLevelBuilder, ILevelBuilder { /*...*/ }
 
 //A class can implement several interfaces in the same way by listing each one, separated by commas:
-public class SomeClass : ISomeInterface1, SomeInterface2 { /* ... */ }
+public class SomeClass : ISomeInterface1, ISomeInterface2 { /* ... */ }
 
 //While you can only have one base class, a class can implement as many interfaces as you want (though this may indicate that an object or class is
-//trying to do too much).
+//trying to do too much.)
 
 //Finally, an interface itself can list other interfaces (but not classes) that it augments or extends:
 public interface IBroaderInterface : INarrowerInterface { /* ... */ }
 
-//When a class implements IBrowserInterface, they will also be on the hook to implement INarrowerInterface.
+//When a class implements IBroaderInterface, they will also be on the hook to implement INarrowerInterface.
 
 //EXPLICIT INTERFACE IMPLEMENTATIONS
 
+//Occasionally, a class implements two different interfaces containing members  with the same name but different meanings, eg:
+public interface IBomb { void BlowUp(); }
+public interface IBalloon { void BlowUp(); }
 
+public class ExplodingBalloon : IBomb, IBalloon
+{
+    public void BlowUp() { /*...*/ }
+}
 
+//This single method is enough to implement both IBomb and IBalloon. If this one method definition is a good fit for both interfaces, you are done.
 
+//On the other hand, in this situation, "blow up" means different things for bombs than it does balloons. When we define ExplodingBalloon's
+//BlowUp method, which one are we referring to?
+
+//If you have control over these interfaces, consider renaming one or the other. We could rename IBomb.BlowUp to Detonate or IBalloon.BlowUp to
+//Inflate. Problem solved.
+
+//But if you don't want to or can't, the other choice is to make a definition for each using an explicit interface implementation:
+public class ExplodingBalloon : IBomb, IBalloon
+{
+    void IBomb.BlowUp() { Console.WriteLine("Kaboom!"); }
+    void IBalloon.BlowUp() { Console.WriteLine("Whoosh!"); }
+}
+
+//By prefacing the method name with the interface name, you can define two versions of BlowUp, one for each interface. Note that the public has been
+//removed. This is required with explicit interface implementations.
+
+//The big surprise is that explicit implementations are detached from their containing class:
+ExplodingBalloon explodingBalloon = new ExplodingBalloon();
+// explodingBalloon.BlowUp(); // COMPILER ERROR!
+
+IBomb bomb = explodingBalloon;
+bomb.BlowUp();
+
+IBalloon balloon = explodingBalloon;
+balloon.BlowUp();
+
+//In this situation, you cannot call BlowUp directly on ExplodingBalloon! Instead, you must store it in a variable that is either IBomb or IBalloon
+//(or cast it to one or the other). Then it becomes available because it is no longer ambiguous.
+
+//If one of the two is more natural for the class, you can choose to do an explicit implementation for only one, leaving the other as the default.
+//If you do this, then the non-explicit implementation is accessible on the object without casting it to the interface type.
+
+//DEFAULT INTERFACE METHODS
+
+//Interfaces allow you to create a default implementation for methods with some restrictions. (If you do not like these restrictions, an abstract
+//base class may be a better fit.) Default implementations are primarily for growing or expanding an existing interface to do more. Imagine having
+//an interface with ten classes that implement the interface. If you want to add a new method or property to this interface, you have to revisit
+//each of the ten implementations to adapt them to the new changes.
+
+//If you can get an interface definition right the first time around, it saves you from this rework. It is worth taking time to try to get interfaces
+//right, but we can never guarantee that. Sometimes, things just need to change.
+
+//Of course, you can just go for it and add the new member to each of the many implementations. This is often a good, clean solution, even though
+//it takes time.
+
+//In other situations, providing a default implementation for a method can be a decent alternative. Imagine you have an interface that a thousand
+//programmers around the world use. If you change it, they'll all need to update their code. A default implementation may save a lot of pain
+//for many people.
+
+//Let's suppose we started with this interface definition:
+public interface ILevelBuilder
+{
+    Level BuildLevel(int levelNumber);
+    int Count { get; }
+}
+
+//If we wanted to build all the levels at once, we might consider adding a Level[] BuildAllLevels() method. Adding this is easy:
+public interface ILevelBuilder
+{
+    Level BuildLevel(int levelNumber);
+    int Count { get; }
+    Level[] BuildAllLevels();
+}
+
+//But the logic for this is pretty standard, and if we can just make a default implementation for BuildAllLevels(), nobody is required to make their
+//own. We can grow the interface almost for free:
+public interface ILevelBuilder
+{
+    Level BuildLevel(int levelNumber);
+    int Count { get; }
+    Level[] BuildAllLevels()
+    {
+        Level[] levels = new Level[Count];
+
+        for (int index = 1; index <= Count; index++)
+            levels[index - 1] = BuildLevel(index);
+
+        return levels;
+    }
+}
+
+//With the default implementation, nobody else will have to write a BuildAllLevels method unless they need something special. But if they do, it is
+//a simple matter of adding a definition for the method in the class.
+
+//A default implementation can use the other members of the interface. We see that above since BuildAllLevels calls both Count and BuildLevel.
+
+//Support Default Interface Methods
+
+//Default interface method implementations are a relatively new thing to C#. When they decided to add this feature, they provided many of the tools
+//needed to do it well. For example, if a single method becomes too big, you can split some of the code into private methods. You can also create
+//protected methods and static methods. Default method implementations are not all that common. You cannot add instance fields. Interfaces cannot
+//contain data themselves.
+
+//Should I Use Default Interface Methods?
+
+//Adding default implementations in an interface was a somewhat controversial change. It is hard for those making widely used interfaces to update
+//every implementing class. The benefits of default implementations are a lifesaver to them. But for many others, the benefits are small, and it
+//serves little value other than to cloud the concept of an interface.
+
+//Should you embrace them and provide one for every method you make, avoid them like the plague, or something in between?
+
+//My recommendation stems from the fact that interfaces are meant to define just the boundary, not the implementation. I suggest skipping default
+//implementations except when many classes implement the interface and when default implementation is nearly always correct for the classes that
+//use the interface.
+
+//Not every interface change can be solved with default method implementations. It only works if you are adding new stuff to an interface. If you are
+//renaming or removing a method, you will just need to fix all the classes that implement the interface.
---
+++ Concepts/Structs.cs
@@ -0,0 +1,122 @@
+﻿//While classes are a great way to create new reference types, C# also lets you make custom value types. New types of this nature are called structs,
+//which is short for structure or data structure. Making a struct is nearly the same as making a class. We have seen many variations on a Point class
+//before, but here is a Point struct:
+public struct Point
+{
+    public float X { get; }
+    public float Y { get; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+//The only code difference is using the struct keyword instead of class. Most aspects of making a struct are the same as making a class. You can add
+//fields, properties, methods, and constructors, along with some other member types we have not discussed yet.
+
+//Using a struct is also nearly the same as using a class:
+Point p1 = new Point(2, 4);
+Console.WriteLine($"({p1.X}. {p1.Y}");
+
+//The critical difference is that structs are value types instead of reference types. This means variables whose type is a struct contain the data
+//where the variable lives, instead of holding a reference that points to the data, as is the case with classes.
+
+//Recall that the variable's contents are copied when passing something between methods (an argument or return value). For a reference type like
+//classes, that means the reference is copied. The calling method and the called method both have their own reference, but both references point
+//to the same object. For a value type like structs, the entire block of data is copied, and each ends up with a full copy of the data. The same is
+//true when assigning a value from one local variable to another or working with expressions.
+
+//Structs are primarily useful for representing small data-related concepts that don't have a lot of behaviour. Representing a 2D point, as we did
+//above, is a good candidate for a struct. A circle, a line, or a matrix could also be good candidates.  In situations where the concept is not a
+//small data-related concept, a class is usually better. Even still, some small data-related concepts are still better than a class. We'll analyze
+//the class vs. struct decision in more depth in a moment.
+
+//MEMORY AND CONSTRUCTORS
+
+//Because structs are value types, memory usage and constructors are two critical ways structs differ from the classes we have been making in the
+//past. Reference types, such as a class, can be null. In these cases, the memory for an object doesn't exist until it is explicitly created by
+//calling a constructor with the new keyword. For value types like structs, we don't have that option. The variable's mere existence means its
+//memory must also exist, even before it has been initialized by a constructor. This model has a lot of implications that may be surprising.
+
+//First, while a constructor can be used to initialize data, invoking a constructor is not always necessary. Consider this struct:
+public struct PairOfInts
+{
+    public int A; // These are public fields, which are usually best to avoid
+    public int B;
+}
+
+//Now, look at this code with a PairOfInts local variable:
+PairOfInts pair;
+pair.A = 2;
+Console.WriteLine(pair.A);
+
+//It calls no constructor but still assign a value to its A field. The pair variable acts like two separate local variables, each of which can be
+//initialized and used like any other local variable but through a shared name.
+
+//Now imagine we add this class into the mix:
+public class PairOfInts
+{
+    public PairOfInts _one;
+    public PairOfInts _two;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_one.A} {_one.B} and {_two.A} {_two.B}");
+    }
+}
+
+//Once again, we can use these structs without calling a constructor. In this case, the structs are initialized to default values by zeroing out
+//their memory, meaning A and B of both _one and _two will be 0 until somebody changed it.
+
+//No matter what constructors you give a struct, they may simply not be called!
+
+//Second, structs will always have a public parameterless constructor. If a class doesn't define any constructors, the compiler automatically generates
+//a parameterless constructor for any class you make. The compiler does the same thing for a struct. For a class, if you define a different
+//constructor, the compiler no longer makes a parameterless constructor. For a struct, the compiler will define a public paramterless constructor
+//anyway. You cannot get rid of the public parameterless constructor yourself if you need it to do something specific.
+
+//Third, field initializers are a bit weird in a struct. Consider this version of PairOfInts:
+public struct PairOfInts
+{
+    public int A = 10;
+    public int B = -2;
+}
+
+//These initializers do not always run when you use a PairOfInts. More specifically:
+// - field and property initializers don't even run if no contructor is called
+// - the compiler-generated constructor runs these initializers only if the struct has no constructors
+// - if you add your own constructors, these initializers will only run as part of constructors YOU have defined, not as part of the compiler-generated
+//   one.
+
+//To ensure the third rule doesn't catch you off guard, you will likely want to define your own parameterless constructor when adding initializers to
+//your fields or properties.
+
+//You don't need to memorize all these rules. Just remember that it can be a tricky area. Don't just assume your code works, but check to ensure it
+//does.
+
+//CLASSES VS. STRUCTS
+
+//Classes and structs have a lot in common, but let's compare the two and describe when you might want to use them.
+
+//The main difference is that classes are reference types and structs are value types. We touched on this in the previous section, but it means
+//struct-typed variables store their data directly, while class-typed variables store a referece, and the actual data lives elsewhere (see level 14).
+
+//This one difference has a lot of rammificatinons, not the least of which is the differences in constructors described in the previous section.
+
+//Another key difference is that structs cannot take on a null value, though we will see a way to pretend in level 32.
+
+//Because structs are value types, reading and writing values to variables involves copying the whole pile of data around, not just a reference.
+//Like with a double, when we copy a value from one variable to another results in a copy:
+PaitOfInts first = new PairOfInts(2, 10);
+PairOfInts second = first;
+
+ //Here, the second will get a copy of both the 2 and the 10 assigned to its fields. The same thing would happen if we passed a PairOfInts to a
+ //method as an argument.
+
+//Additionally, inheritance does not work well when copying value types around (do a web search for 'object slicing'), so structs do not support it.
+//A struct cannot pick a base class (they all derive from ValueType, which derives from object). Structs, however, are allowed to implement interfaces.
+
+//Equality is also different for structs. As we saw in level 14, value types have value semantics - two things are equal if all of their data
+//members are equal. Any struct you create will automatically have value semantics. The Equals method and the == and != operators are automatically
+//defined to compare the struct's fields for equality.
