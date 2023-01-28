3b556fd1a8701e081eda28f7c18234626c3616a5
jamesa <james.aslett@directlife.co.uk>
2023-01-28 22:47:13 +0000

    Completed TheSieve

--- Challenges/TheSieve.cs
+++ Challenges/TheSieve.cs
@@ -1,10 +1,38 @@
-﻿//Create a Sieve class with a public bool IsGood(int number) method. This class needs a constructor with a delegate parameter that can be invoked later within the IsGood method. Hint: You can make your own delegate type or use Func<int, bool>
+﻿
+Console.WriteLine("Please choose from the following:");
+Console.WriteLine("1: Even numbers");
+Console.WriteLine("2: Positive numbers");
+Console.WriteLine("3: Multiples of ten");
+int choice = Convert.ToInt32(Console.Read());
 
-//Define methods with an int parameter and a bool return type for the following:
-    //1: returns true for even numbers
-    //2: returns true for positive numbers
-    //3: returns true for multiples of 10
+Sieve sieve = choice switch
+{
+    1 => new Sieve(IsEven),
+    2 => new Sieve(IsPositive),
+    3 => new Sieve(IsMultipleOfTen),
+    _ => throw new NotImplementedException()
+};
 
-//Create a program that asks the user to pick one of those three filters, constructs a new Sieve instance by passing in one of thoses methods as a parameter, and then ask the user to enter numbers repeatedly, displaying whether the number is good or bad depending on the filter in use
+while (true)
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string goodOrBad = sieve.IsGood(number) ? "good" : "bad";
+    Console.WriteLine($"That number is {goodOrBad}.");
+}
 
-//Answer this question: Describe how you could have solved this problem with inheritance and polymorphism. Which solution seems more straightforward to you, and why?
+bool IsEven(int number) => number % 2 == 0;
+bool IsPositive(int number) => number > 0;
+bool IsMultipleOfTen(int number) => number % 10 == 0;
+
+public class Sieve
+{
+    private readonly Func<int, bool> _decisionFunction;
+
+    public Sieve(Func<int, bool> decisionFunction) => _decisionFunction = decisionFunction;
+
+    public bool IsGood(int number)
+    {
+        return _decisionFunction(number);
+    }
+}
