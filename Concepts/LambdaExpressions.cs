cb948514d399ceae7089a4bd53e2157d34d2bdfc
jamesa <james.aslett@directlife.co.uk>
2023-02-04 22:36:15 +0000

    Update

--- Concepts/LambdaExpressions.cs
+++ Concepts/LambdaExpressions.cs
@@ -35,4 +35,68 @@
 //Yet, many of the other elements of this definition are gone. No prive. No static. No stated return type. No name. No parentheses around the parameters. No type listed for the paramter. Plus, we used the variable name n instead of number.
 
 //A lambda expression defines a single-use method inline, right where it is needed. To prevent the code from getting ugly, everything in a lambda expression uses a minimalistic form:
-//- The accessibility level goes away because you cannot 
+//- The accessibility level goes away because you cannot reuse a lambda expresson elsewhere
+//- The compiler infers the return type and parameter types from the surrounding context. Since the countFunction parameter is a Func<int, bool>, it is easy for the compiler to infer that n must be an int, and the expression must return a bool
+//- The name is gone because it is a single-use method and does not need to be used again
+//- The parentheses are gone just to make the code simpler
+
+//Using the name n instead of number also makes the code shorter. Generally, more descriptive names are better, but C# programmers tend to use concise names in a lambda expression. When a variable is only used in the following few characters, the downsides of a short name are not nearly as significant as they are in a 30-line method.
+
+//We can do some pretty cool things with little code using a combination of lambda expressions and delegates. This counts the number of positive integers:
+//int positives = Count(numbers, n => n > 0);
+
+//This counts positive three-digit integers:
+//int threeDigitCount = Count(numbers, n => n >= 100 && n < 1000);
+
+//Lambda expressions are different enough from normal methods that it may require some time to adjust. But with a bit of practice, you will find them a simple but powerul tool.
+
+//The Origin of the nane Lambda
+//You may be wondering why this is called a lambda expression. The name comes from lambda calculus. Lambda calculus is a type of functon-oriented math - almost a mathematical programming language. The nature of lambda expressions and delegates is heavily inspired by lambda calculus. In lambda calculus, the name lambda comes from its usage of the Greek letter lambda.
+
+//Multiple and zero parameters
+//Our lambda expressions so far have all had a single parameter. Let's talk about lamdba expressions with zero or many parameters. When you have zero or multiple parameters, the parentheses come back.
+
+//A lambda expression with two parameters looks like this:
+//(a, b) => a + b
+
+//A lambda expression with no parameters looks like this:
+//() => 4
+
+//These two cases require parentheses, but parentheses are always an option:
+//(n) => n % 2 == 0
+
+//When type inference fails
+//Sometimes, the compiler cannot infer the parameter types in a lambda expression. If you encounter this, you can name the types explicitly, as you might for a normal metohd:
+//(int n) => n % 2 == 0
+
+//Or:
+//(string a, string b) => a + b
+
+//If the compiler can't correctly infer the return type of a method, you can write out the return type before the parentheses that contain the parameters like this:
+//bool (n) => n % 2 == 0
+
+//Or:
+//bool (int n) => n % 2 == 0
+
+//In all of these cases, parentheses are required.
+
+//Discards
+//Lambdas are often used in places where the code demands certain parameters but where you may not need all of them. If so, you can use discards for those parameters with either of the following two forms:
+//(_, _) => 1
+//(int _, int _) => 1
+
+//Lambda Statements
+//Most of the time, when you want a simple single-use method, an expression is all you need, and lambda expressions are a good fit. You can use a lambda statement in the rare cases where a statement or several statemenrs are required. Lambda expressions and lambda statements are both sometimes referred to by the shorter catch-all name lambda.
+
+//Making a statement lambda is simple enough. Replace the expression body with a block body:
+//Count(numbers, n => { return n % 2 == 0; });
+
+//Or this:
+//Count(numbers, n => { Console.WriteLine(n); return n % 2  == 0; };
+
+//Closures
+//Lambdas and local functions can do something normal methods can't do. Consider this code:
+//int threshold = 3;
+//Count(numbers, x => x < threshold);
+
+//The lambda expression has one parameter: x. However, it can use the local variables of the method that contains it. Here, it uses threshold. Lambda expressions and local functions
