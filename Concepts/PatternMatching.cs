3ef15e94e54f1ffe8af213f79c78d7f60e55928a
jamesa <james.aslett@directlife.co.uk>
2023-05-10 12:46:21 +0100

    Pattern Matching continued

--- Concepts/PatternMatching.cs
+++ Concepts/PatternMatching.cs
@@ -17,3 +17,33 @@
 //-In the game of Rock-Paper-Scissors, given the player's choices, which player won?
 
 //You can solve these problems with the venerable if statement, but C# provides another tool designed specifically for these situations: pattern matching. Pattern matching lets you define categorization rules to determine which category an object fits in. You can use pattern matching in switch expressions, switch statements, and the is keyword.
+
+//THE CONSTANT PATTERN AND THE DISCARD PATTERN
+//We got our first glimpse in Level 10 when we made our pirate-themed menu:
+//string response = choice switch
+//{
+//  1 => "Ye rest and recover your health.",
+//  2 => "Raiding the port town get ye 50 gold doubloons.",
+//  3 => "The wind is at your back; the open horizon ahead.",
+//  4 => "Tis but a baby Kraken, but still eats toy boats.",
+//  _ => "Apologies. I do not know that one."
+//};
+
+//This level was our introduction to patterns, though we didn't know it at the time.
+
+//In a switch expression, each arm is defined by a pattern on the left, followed by the => operator, followed by the expression to evaluate if the pattern is a match (pattern expression => evaluation expression). Each pattern is a rule that determines if the object under consideration fits into the category or not. The switch expression above uses the two most basic patterns: the constant pattern and the discard pattern.
+
+//The first four lines show the constant pattern, which decides if there is a match based on whether the item exactly equals some constant value, like the literals 1, 2, 3 or 4.
+
+//The last switch arm uses the discard pattern: _. This pattern is a catch-all pattern, matching anything and everything. In C#, a single underscore usually represents a discard, signifying that what goes in that spot does not matter. Here, it indicates that there is nothing to check - that there are no constraints or rules for matching the pattern. Because it matches anything, when it shows up, it should always be the very last pattern.
+
+//But these two patterns are only the beginning.
+
+//THE MONSTER SCORING PROBLEM
+//Having a realistic and complex problem can help illustrate the different patterns we will be learning. In this level, we will use the following problem: in a game where the player fights monsters, given some Monster instance, determine how many points to award the player for defeating it. In code, we might write this problem like so:
+//int ScoreFor(Monster monster)
+//{
+//  // ...
+//}
+
+//Let's also define what a Monster is:
